3c5b6e326ddbce004a614ae51e0b628778ed4f96
Andreas König <andreas.koenig@berg-turbenthal.ch>
2011-07-21 08:22:33 +0200

    improved log messages and protected private methods

--- lib/ghost_reader.rb
+++ lib/ghost_reader.rb
@@ -19,10 +19,225 @@ module GhostReader
       if defined?(PhusionPassenger)
         # In Passenger load data in foreground
         @store=load_yaml_from_ghostwriter
+        log "Data in Passenger-Mode directly loaded"
       else
         # Elsewhere load data in Background
         call_server
       end
+    end
+
+    def load_yaml_from_ghostwriter
+      wait_bg_thread()
+      response = call_get_on_ghostwriter
+      case response
+        when Net::HTTPSuccess
+          return YAML.load(response.body.to_s)
+      end
+      {}
+    end
+
+    def push_all_backend_data
+      wait_bg_thread()
+      unless @default_backend && @default_backend.respond_to?(:get_all_data)
+        puts "Default Backend not support reading all Data"
+        return
+      end
+      miss_data={}
+
+      @default_backend.get_all_data.each_pair do |locale, entries|
+        collect_backend_data(entries, locale, [], miss_data)
+      end
+      last_res=call_put_on_ghostwriter({}, miss_data)
+      unless (last_res.kind_of?(Net::HTTPSuccess) ||
+              last_res.kind_of?(Net::HTTPNotModified))
+        puts "Unexpected Answer from Server"
+        puts "#{last_res.code}: #{last_res.message}"
+      end
+    end
+
+    def collect_backend_data(entries, locale, base_chain, miss_data)
+      entries.each_pair do |key, sub_entries|
+        key_data=[base_chain, [key.to_s]].flatten
+        case sub_entries
+          when String
+            key_string=key_data.join '.'
+            key_data=miss_data[key_string]
+            unless key_data
+              key_data={'default'=>{}, 'count'=>{}}
+              miss_data[key_string]=key_data
+            end
+            key_data['default'][locale.to_s]=sub_entries
+          when Hash
+            collect_backend_data sub_entries, locale, key_data, miss_data
+        end
+      end
+    end
+
+    def lookup(locale, key, scope = [], options = {})
+      init_translations unless initialized?
+      call_server
+
+      keys = I18n.normalize_keys(locale, key, scope, options[:separator])
+      filtered_options=options.reject { |key, value| key.to_sym==:scope }
+      full_key=keys[1, keys.length-1].join('.')
+      lookup_key(locale, keys, full_key, filtered_options)
+    end
+
+    def available_locales
+      init_translations unless initialized?
+      call_server
+      locales=[]
+      if @default_backend
+        locales.concat @default_backend.available_locales
+      end
+      if @store
+        locales.concat @store.keys
+      end
+      locales.uniq
+    end
+
+    protected
+      # Simple logs messages to console if enabled
+    def log(message)
+      if @trace
+        @trace.call "#{Time.now.strftime("%Y-%m-%d %H:%M:%S")} [#{Thread.current}]: Ghost_Reader: #{message}"
+      end
+    end
+
+    def lookup_key(locale, keys, full_key, filtered_options)
+      found_value=keys.inject(@store) do |result, _key|
+        _key = _key.to_sym
+        unless result.is_a?(Hash) && result.has_key?(_key)
+          inc_miss locale.to_s, full_key.to_s, filtered_options
+          return @default_backend.lookup locale, full_key
+        end
+        result = result[_key]
+        result
+      end
+      if found_value.is_a? Hash
+        default_values = @default_backend.lookup(locale, full_key)
+        if default_values
+          found_value=default_values.deep_merge(found_value)
+          inc_miss locale.to_s, full_key.to_s, filtered_options
+        end
+      end
+      inc_hit full_key.to_s, filtered_options
+      found_value
+    end
+
+    # contact server and exchange data if last call is more than @wait_time
+    #seconds
+    def call_server
+      if @bg_thread
+        #log "BG-Thread: #{@bg_thread}"
+        # dont start more than one background_thread
+        return if @bg_thread.alive?
+          # get the values from the last call
+        if @bg_thread[:store]
+          @store=@bg_thread[:store]
+          @last_version=@bg_thread[:last_version]
+          log "New data from Server activated"
+        end
+        @bg_thread = nil
+        #return
+      end
+      return if Time.now.to_i-@last_server_call<@wait_time
+
+        # take the needed Values and give it to the thread
+      misses=@misses
+      hits=@hits
+        # make new empty values for collecting statistics
+      @hits={}
+      @misses={}
+      @last_server_call=Time.now.to_i
+      @bg_thread=Thread.new(misses, hits) do
+        begin
+          miss_data = calc_miss_data(misses)
+          hit_data = calc_hit_data(hits)
+
+          if miss_data.size>0 || hit_data.size>0
+            res = call_put_on_ghostwriter(hit_data, miss_data)
+          else
+            res = call_get_on_ghostwriter()
+          end
+          case res
+            when Net::HTTPSuccess
+              Thread.current[:store]=YAML.load(res.body.to_s).deep_symbolize_keys
+              Thread.current[:last_version]=res["last-modified"]
+          end
+        rescue Object => ex
+          puts "Exception ============\n#{ex.inspect}\n========================\n"
+        end
+      end
+    end
+
+      # counts a hit to a key
+    def inc_hit(key, options)
+      if @hits[key]
+        @hits[key]+=1
+      else
+        @hits[key]=1
+      end
+    end
+
+      # counts a miss to a key and a locale
+    def inc_miss(locale, key, options)
+      if @misses[key]
+        key_hash=@misses[key]
+      else
+        key_hash={}
+        @misses[key]=key_hash
+      end
+      if (key_hash[locale])
+        key_hash[locale]+=1
+      else
+        key_hash[locale]=1
+      end
+    end
+
+    def call_get_on_ghostwriter
+      url=URI.parse(@url)
+      req=Net::HTTP::Get.new(url.path)
+      req['If-Modified-Since']=@last_version if @last_version
+      log "Get start"
+      res = Net::HTTP.new(url.host, url.port).start do |http|
+        http.request(req)
+      end
+      log "Get returned with #{res.code}"
+      res
+    end
+
+    def call_put_on_ghostwriter(hits, miss_data)
+      res=nil
+      while (hits.size>0 || miss_data.size>0) &&
+              (res==nil ||
+                      res.kind_of?(Net::HTTPSuccess)||
+                      res.kind_of?(Net::HTTPNotModified))
+        call_entry_count=0
+        part_hits={}
+        part_miss={}
+        while (call_entry_count<@max_packet_size && hits.size>0)
+          entry=hits.shift
+          part_hits[entry[0]]=entry[1]
+          call_entry_count+=1
+        end
+        while (call_entry_count<@max_packet_size && miss_data.size>0)
+          entry=miss_data.shift
+          part_miss[entry[0]]=entry[1]
+          call_entry_count+=1
+        end
+        url=URI.parse(@url)
+        req=Net::HTTP::Post.new(url.path)
+        req['If-Modified-Since']=@last_version
+        req.set_form_data({:hits=>part_hits.to_json,
+                           :miss=>part_miss.to_json})
+        log "Post start"
+        res = Net::HTTP.new(url.host, url.port).start do |http|
+          http.request(req)
+        end
+        log "Post returned with #{res.code}"
+      end
+      res
     end
 
       # Add a new default value
@@ -118,215 +333,13 @@ module GhostReader
       hit_data
     end
 
-    def load_yaml_from_ghostwriter
-      response = call_get_on_ghostwriter
-      case response
-        when Net::HTTPSuccess
-          return YAML.load(response.body.to_s)
-      end
-      {}
-    end
-
-    def call_get_on_ghostwriter
-      url=URI.parse(@url)
-      req=Net::HTTP::Get.new(url.path)
-      req['If-Modified-Since']=@last_version if @last_version
-      log "Get start"
-      res = Net::HTTP.new(url.host, url.port).start do |http|
-        http.request(req)
-      end
-      log "Get returned with #{res.code}"
-      res
-    end
-
-    def call_put_on_ghostwriter(hits, miss_data)
-      res=nil
-      while (hits.size>0 || miss_data.size>0) &&
-              (res==nil ||
-                      res.kind_of?(Net::HTTPSuccess)||
-                      res.kind_of?(Net::HTTPNotModified))
-        call_entry_count=0
-        part_hits={}
-        part_miss={}
-        while (call_entry_count<@max_packet_size && hits.size>0)
-          entry=hits.shift
-          part_hits[entry[0]]=entry[1]
-          call_entry_count+=1
-        end
-        while (call_entry_count<@max_packet_size && miss_data.size>0)
-          entry=miss_data.shift
-          part_miss[entry[0]]=entry[1]
-          call_entry_count+=1
-        end
-        url=URI.parse(@url)
-        req=Net::HTTP::Post.new(url.path)
-        req['If-Modified-Since']=@last_version
-        req.set_form_data({:hits=>part_hits.to_json,
-                           :miss=>part_miss.to_json})
-        log "Post start"
-        res = Net::HTTP.new(url.host, url.port).start do |http|
-          http.request(req)
-        end
-        log "Post returned with #{res.code}"
-      end
-      res
-    end
-
-    def push_all_backend_data
-      unless @default_backend && @default_backend.respond_to?(:get_all_data)
-        puts "Default Backend not support reading all Data"
-        return
-      end
-      miss_data={}
-
-      @default_backend.get_all_data.each_pair do |locale, entries|
-        collect_backend_data(entries, locale, [], miss_data)
-      end
-      last_res=call_put_on_ghostwriter({}, miss_data)
-      unless (last_res.kind_of?(Net::HTTPSuccess) ||
-              last_res.kind_of?(Net::HTTPNotModified))
-        puts "Unexpected Answer from Server"
-        puts "#{last_res.code}: #{last_res.message}"
+    def wait_bg_thread
+      bg_thread = @bg_thread
+      if bg_thread
+        bg_thread.join
       end
     end
 
-    def collect_backend_data(entries, locale, base_chain, miss_data)
-      entries.each_pair do |key, sub_entries|
-        key_data=[base_chain, [key.to_s]].flatten
-        case sub_entries
-          when String
-            key_string=key_data.join '.'
-            key_data=miss_data[key_string]
-            unless key_data
-              key_data={'default'=>{}, 'count'=>{}}
-              miss_data[key_string]=key_data
-            end
-            key_data['default'][locale.to_s]=sub_entries
-          when Hash
-            collect_backend_data sub_entries, locale, key_data, miss_data
-        end
-      end
-    end
-
-      # contact server and exchange data if last call is more than @wait_time
-      #seconds
-    def call_server
-      if @bg_thread
-        # dont start more than one background_thread
-        return if @bg_thread.alive?
-        # get the values from the last call
-        if @bg_thread[:store]
-          @store=@bg_thread[:store]
-          @last_version=@bg_thread[:last_version]
-        end
-        @bg_thread = nil
-        #return
-      end
-      return if Time.now.to_i-@last_server_call<@wait_time
-
-      # take the needed Values and give it to the thread
-      misses=@misses
-      hits=@hits
-        # make new empty values for collecting statistics
-      @hits={}
-      @misses={}
-      @last_server_call=Time.now.to_i
-
-      @bg_thread=Thread.new(misses, hits) do
-        begin
-          miss_data = calc_miss_data(misses)
-          hit_data = calc_hit_data(hits)
-
-          if miss_data.size>0 || hit_data.size>0
-            res = call_put_on_ghostwriter(hit_data, miss_data)
-          else
-            res = call_get_on_ghostwriter()
-          end
-          case res
-            when Net::HTTPSuccess
-              Thread.current[:store]=YAML.load(res.body.to_s).deep_symbolize_keys
-              Thread.current[:last_version]=res["last-modified"]
-          end
-        rescue Object => ex
-          puts "Exception ============\n#{ex.inspect}\n========================\n"
-        end
-      end
-
-    end
-
-      # counts a hit to a key
-    def inc_hit(key, options)
-      if @hits[key]
-        @hits[key]+=1
-      else
-        @hits[key]=1
-      end
-    end
-
-      # counts a miss to a key and a locale
-    def inc_miss(locale, key, options)
-      if @misses[key]
-        key_hash=@misses[key]
-      else
-        key_hash={}
-        @misses[key]=key_hash
-      end
-      if (key_hash[locale])
-        key_hash[locale]+=1
-      else
-        key_hash[locale]=1
-      end
-    end
-
-    def lookup(locale, key, scope = [], options = {})
-      init_translations unless initialized?
-      call_server
-
-      keys = I18n.normalize_keys(locale, key, scope, options[:separator])
-      filtered_options=options.reject { |key, value| key.to_sym==:scope }
-      full_key=keys[1, keys.length-1].join('.')
-      lookup_key(locale, keys, full_key, filtered_options)
-    end
-
-    def lookup_key(locale, keys, full_key, filtered_options)
-      found_value=keys.inject(@store) do |result, _key|
-        _key = _key.to_sym
-        unless result.is_a?(Hash) && result.has_key?(_key)
-          inc_miss locale.to_s, full_key.to_s, filtered_options
-          return @default_backend.lookup locale, full_key
-        end
-        result = result[_key]
-        result
-      end
-      if found_value.is_a? Hash
-        default_values = @default_backend.lookup(locale, full_key)
-        if default_values
-          found_value=default_values.deep_merge(found_value)
-          inc_miss locale.to_s, full_key.to_s, filtered_options
-        end
-      end
-      inc_hit full_key.to_s, filtered_options
-      found_value
-    end
-
-    def available_locales
-      init_translations unless initialized?
-      call_server
-      locales=[]
-      if @default_backend
-        locales.concat @default_backend.available_locales
-      end
-      if @store
-        locales.concat @store.keys
-      end
-      locales.uniq
-    end
-    # Simple logs messages to console if enabled
-    def log(message)
-      if @trace
-        @trace.call "#{Time.now.strftime("%Y-%m-%d %H:%M:%S")}: Ghost_Reader: #{message}"
-      end
-    end
 
   end
   if defined?(Rails)
